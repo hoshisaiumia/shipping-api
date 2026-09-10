2804de74765e1d1f8166dec1efdcacf68b47636e
Hanna <hannaseraphim@gmail.com>
2026-09-10 15:09:49 -0300

    feat(route): health route
    Adds health route to check API status

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 
+app.get("/health", (req: express.Request, res: express.Response) => {
+  res.status(200).json({ message: "ok", status: 200 });
+});
+
 app.listen(PORT, () => {
   Logger.info(`Server running on port ${PORT}`);
 });
