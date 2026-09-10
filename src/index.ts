fbb259fd6bef6949790d73411818e6fd3b0a9914
Hanna <hannaseraphim@gmail.com>
2026-09-10 02:19:11 -0300

    feat(express): create express base server
    Features a basic working express server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
-import express from "express"
+import express from "express";
+import "dotenv-flow/config";
+import { Logger } from "./utils/logger.js";
 
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  Logger.info(`Server running on port ${PORT}`);
+});
